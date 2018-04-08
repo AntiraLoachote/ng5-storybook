32e10d289dcbf70fd6f53043cd89093ae7bf4a7b
AntiraLoachote <antirapan26@gmail.com>
2018-04-08 16:27:42 +0700

    add first commit

---
+++ .storybook/config.js
@@ -0,0 +1,28 @@
+// import { configure } from '@storybook/angular';
+
+// function loadStories() {
+//     require('../src/stories/index.ts');
+// }
+
+// configure(loadStories, module);
+
+import { configure } from '@storybook/angular';
+import { setOptions } from '@storybook/addon-options';
+// import addCssWarning from '../src/cssWarning';
+
+// addCssWarning();
+
+setOptions({
+    hierarchyRootSeparator: /\|/,
+});
+
+function loadStories() {
+    // put welcome screen at the top of the list so it's the first one displayed
+    require('../src/stories');
+
+    // automatically import all story ts files that end with *.stories.ts
+    const req = require.context('../src/stories', true, /\.stories\.ts$/);
+    req.keys().forEach(filename => req(filename));
+}
+
+configure(loadStories, module);
